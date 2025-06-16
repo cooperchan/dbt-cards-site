5cb4a487aeda5842a9ae60673a127405f968a349
R Cooper <reahanna.cooper@gmail.com>
2025-06-15 21:54:53 -0400

    Rolling back to version with synced flow

--- script.js
+++ script.js
@@ -194,16 +194,13 @@ function createCardElement(frontText, backText, title = '', layerIndex = 0, cate
       // Desktop: Hover flips, click unflips, second click advances
       let wasFlipped = false;
 
-card.addEventListener('mouseenter', () => {
-  if (!card.classList.contains('flipped')) {
-    card.classList.add('hover-preview');
-  }
-});
+      card.addEventListener('mouseenter', () => {
+        card.classList.add('flipped');
+      });
 
-card.addEventListener('mouseleave', () => {
-  card.classList.remove('hover-preview');
-});
-
+      card.addEventListener('mouseleave', () => {
+        card.classList.remove('flipped');
+      });
 
       inner.addEventListener('click', (e) => {
         e.stopPropagation();
