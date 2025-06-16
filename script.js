00fe2206db801e160641208d73438901cfdad9c0
R Cooper <reahanna.cooper@gmail.com>
2025-06-15 21:37:38 -0400

    Hover preview

--- script.js
+++ script.js
@@ -194,13 +194,16 @@ function createCardElement(frontText, backText, title = '', layerIndex = 0, cate
       // Desktop: Hover flips, click unflips, second click advances
       let wasFlipped = false;
 
-      card.addEventListener('mouseenter', () => {
-        card.classList.add('flipped');
-      });
+// Desktop hover effect that doesn’t interfere with click flow
+card.addEventListener('mouseenter', () => {
+  if (!card.classList.contains('flipped')) {
+    card.classList.add('hover-preview');
+  }
+});
+card.addEventListener('mouseleave', () => {
+  card.classList.remove('hover-preview');
+});
 
-      card.addEventListener('mouseleave', () => {
-        card.classList.remove('flipped');
-      });
 
       inner.addEventListener('click', (e) => {
         e.stopPropagation();
